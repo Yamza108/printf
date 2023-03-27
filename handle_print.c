b1a7bde277627fe52fc398620553c7ffa64fa427
Successmind12 <106842379+Successmind12@users.noreply.github.com>
2023-03-27 15:06:24 +0100

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * handle_print - Prints an argument based on its type
- * @fmt: Formatted string in which to print the arguments.
- * @list: List of arguments to be printed.
+ * handle_print - Prints an argument based on its datatype
+ * @fmt:  Formatted string in which to print the arguments.
+ * @list: Represents list of arguments to be printed.
  * @ind: ind.
- * @buffer: Buffer array to handle print.
+ * @buffer: Buffer array of char datatype to handle print.
  * @flags: Calculates active flags
  * @width: get width.
  * @precision: Precision specification
