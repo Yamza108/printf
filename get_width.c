ebeba8470f0831f132fba3c58946d3a8aff968f5
Successmind12 <106842379+Successmind12@users.noreply.github.com>
2023-03-27 14:59:59 +0100

    Update get_width.c

--- get_width.c
+++ get_width.c
@@ -3,7 +3,7 @@
 /**
  * get_width - Calculates the width for printing
  * @format: Formatted string in which to print the arguments.
- * @i: List of arguments to be printed.
+ * @i: Pointer variable representing list of arguments to be printed.
  * @list: list of arguments.
  *
  * Return: width.
