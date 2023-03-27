a5216a5268c279fcbabd131c295bd921b4d2aae7
Successmind12 <106842379+Successmind12@users.noreply.github.com>
2023-03-27 18:05:41 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -2,7 +2,7 @@
 
 /**
  * is_printable - Evaluates if a char is printable
- * @c: Char to be evaluated.
+ * @c: Character to be evaluated.
  *
  * Return: 1 if c is printable, 0 otherwise
  */
@@ -15,10 +15,10 @@ int is_printable(char c)
 }
 
 /**
- * append_hexa_code - Append ascci in hexadecimal code to buffer
- * @buffer: Array of chars.
+ * append_hexa_code - Append ASCII in hexadecimal code to buffer
+ * @buffer: Array of characters.
  * @i: Index at which to start appending.
- * @ascii_code: ASSCI CODE.
+ * @ascii_code: ASCII CODE.
  * Return: Always 3
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
@@ -38,8 +38,8 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 }
 
 /**
- * is_digit - Verifies if a char is a digit
- * @c: Char to be evaluated
+ * is_digit - Verifies if a character is a digit
+ * @c: Character to be evaluated
  *
  * Return: 1 if c is a digit, 0 otherwise
  */
